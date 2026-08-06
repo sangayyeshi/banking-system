183c5078d2c4ea3b159cd3b9f82ca1d83ca9ef9c
sangayyeshi <sangayyeshi19@gmail.com>
2026-08-07 01:54:52 +0300

    make change on application yml in all sevice

--- config-server/src/test/java/com/banking/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/banking/configserver/ConfigServerApplicationTests.java
@@ -1,12 +1,13 @@
 package com.banking.configserver;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
 
 @SpringBootTest
 class ConfigServerApplicationTests {
 
-    @Test
+    @org.testng.annotations.Test
     void contextLoads() {
     }
 
--- message-service/src/main/java/com/banking/messageservice/MessageServiceApplication.java
+++ message-service/src/main/java/com/banking/messageservice/MessageServiceApplication.java
@@ -1,6 +1,6 @@
 package com.banking.messageservice;
 
-import jakarta.validation.constraints.Email;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
