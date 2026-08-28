f026a77e322e0b64a5fe388c057f61a0f13a5079
sangayyeshi <sangayyeshi19@gmail.com>
2026-08-28 10:47:46 +0300

    fixed the welcome email bugs

--- auth-service/src/main/java/com/banking/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/banking/authservice/AuthServiceApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+@EnableFeignClients
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
--- message-service/src/main/java/com/banking/messageservice/MessageServiceApplication.java
+++ message-service/src/main/java/com/banking/messageservice/MessageServiceApplication.java
@@ -6,9 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+
+
 public class MessageServiceApplication {
 
     public static void main(String[] args) {
